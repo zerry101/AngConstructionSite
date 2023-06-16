c0557bc3494963340a86f9147ce8f06e7e2b318b
zerry101 <macone394@gmail.com>
2023-06-16 19:31:41 +0530

    added rates form

--- src/app/pages/home/rates/rates.component.ts
+++ src/app/pages/home/rates/rates.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {  FormGroup, FormBuilder,Validators } from '@angular/forms';
+import { globalConstants } from 'src/app/Shared/constants';
+
+
 
 @Component({
   selector: 'app-rates',
@@ -6,5 +10,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./rates.component.scss']
 })
 export class RatesComponent {
+
+  constructor(public fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.setupForm();
+  }
+
+  contactForm: FormGroup = new FormGroup({});
+
+  submitted:boolean=false;
+
+  setupForm() {
+    this.contactForm = this.fb.group({
+      Name: ["",[Validators.required]],
+      Email: ["",[Validators.required,Validators.pattern(globalConstants.EMAIL)]],
+      PhoneNumber: ["",[Validators.required,Validators.pattern(globalConstants.CONTACT_NO)]],
+    })
+
+
+  }
+
+  onsubmit(){
+
+    this.submitted=true;
+    if(this.contactForm.invalid){
+      return false;
+    }
+    else{
+      console.log(this.contactForm.value);
+      this.submitted?this.contactForm.reset():false;
+      return;
+    }
+
+  }
+
+  get NameControl() {
+    return this.contactForm.get('Name');
+  }
+
+  get EmailControl() {
+    return this.contactForm.get('Email');
+  }
+
+  get PhoneNumberControl() {
+    return this.contactForm.get('PhoneNumber');
+  }
+
+
 
 }
